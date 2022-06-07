28448b90ad795a083043d8df83d5a78283be7f86
Ajuka David <ajuka.zephiniah@gmail.com>
2022-06-07 21:09:56 +0100

    changed firebase configuration setup

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAeceXieSIQErTCGd5lEuDyYOVX1Se_DdQ",
-  authDomain: "auth-41d75.firebaseapp.com",
-  projectId: "auth-41d75",
-  storageBucket: "auth-41d75.appspot.com",
-  messagingSenderId: "433103566584",
-  appId: "1:433103566584:web:b95087ff08a68487f22d0d"
-};
+  apiKey: 'AIzaSyAeceXieSIQErTCGd5lEuDyYOVX1Se_DdQ',
+  authDomain: 'auth-41d75.firebaseapp.com',
+  projectId: 'auth-41d75',
+  storageBucket: 'auth-41d75.appspot.com',
+  messagingSenderId: '433103566584',
+  appId: '1:433103566584:web:b95087ff08a68487f22d0d'
+}
 
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig)
 
-createApp(App).use(router).use(app).mount('#app')
+createApp(App).use(router).mount('#app')
